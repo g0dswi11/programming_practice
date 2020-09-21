71d9bddb8d3efd589b7dacb791861950009fc1e4
Vadim <retroarefobia@gmail.com>
2020-09-21 21:21:28 +0300

    Add task 06

---
+++ Practice/06/C++/6_roots_of_the_equation.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+
+/* Задание 6 - Корни уравнения */
+
+using namespace std;
+ 
+int main()
+{
+	cout << "6. Корни уравнения" << endl;
+    double a,b,c,x;
+    cout << "Введите значение a: ";
+    cin >> a;
+    cout << "Введите значение b: ";
+    cin >> b;
+    cout << "Введите значение c: ";
+    cin >> c;
+    if((b*b - 4*a*c) >= 0)
+    {
+        x = ( -1*b + sqrt(b*b - 4*a*c) ) / (2 * a);
+        cout << "x1: " << x << endl;
+        x = ( -1*b - sqrt(b*b - 4*a*c) ) / (2 * a);
+        cout << "x2: " << x << endl;
+    }
+    else if((b*b - 4*a*c) == 0) 
+    {
+    	x = (-1*b) / (2*a);
+    	cout << "x: " << x << endl;
+    }
+    else
+    {
+        cout << "Дискриминант меньше 0, корней нет." << endl;
+    }
+ 
+    return 0;
+}
