15f769033504a8964498812d46fa4ac099600c50
Vadim <retroarefobia@gmail.com>
2020-09-23 10:10:44 +0300

    add task 08

---
+++ Practice/08/C++/8_area_of_a_triangle.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <cmath>
+
+/* Задание 8 - Калькулятор */
+
+using namespace std;
+
+bool validate(const float a, const char c, const float b)
+{
+    if (c != '+' && c != '-' && c != '*' && c != '/')
+    {
+        cout << "Операция не поддерживается!" << endl;
+        return false;
+    }
+    if (cin.fail())
+    {
+        cin.clear();
+        cin.ignore(32767, '\n');
+        return false;
+    }
+    cin.ignore(32767, '\n');
+    return true;
+}
+
+float calc(const float a, const char c, const float b)
+{
+    switch (c)
+    {
+    case '+':
+        return a + b;
+    case '-':
+        return a - b;
+    case '*':
+        return a * b;
+    case '/':
+        return a / b;
+    default:
+        throw logic_error("Операция не поддерживается!");
+    }
+}
+
+int main()
+{
+    cout << "8. Калькулятор" << endl;
+    float a, b;
+    char c;
+    while (1)
+    {
+        cin.clear();
+        cin >> a >> c >> b;
+        if (validate(a, c, b))
+        {
+            cout << calc(a, c, b) << endl;
+            break;
+        }
+        else
+        {
+            cout << "Ошибка ввода: " << endl;
+            continue;
+        }
+    }
+
+    return 0;
+}
---
+++ Practice/08/Python/8_calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+# Задание 8 - Калькулятор
+
+while 1:
+    try:
+        print(eval(input())); break
+    except Exception as e: print('Ошибка: ', e); continue
