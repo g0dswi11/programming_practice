0b975f05ba62452211dbc90647131b1dfc805119
Vadim <retroarefobia@gmail.com>
2020-09-23 08:31:17 +0300

    reformat code

--- Practice/06/C++/6_roots_of_the_equation.cpp
+++ Practice/06/C++/6_roots_of_the_equation.cpp
@@ -4,33 +4,33 @@
 /* Задание 6 - Корни уравнения */
 
 using namespace std;
- 
+
 int main()
 {
-	cout << "6. Корни уравнения" << endl;
-    double a,b,c,x;
+    cout << "6. Корни уравнения" << endl;
+    double a, b, c, x;
     cout << "Введите значение a: ";
     cin >> a;
     cout << "Введите значение b: ";
     cin >> b;
     cout << "Введите значение c: ";
     cin >> c;
-    if((b*b - 4*a*c) >= 0)
+    if ((b * b - 4 * a * c) >= 0)
     {
-        x = ( -1*b + sqrt(b*b - 4*a*c) ) / (2 * a);
+        x = (-1 * b + sqrt(b * b - 4 * a * c)) / (2 * a);
         cout << "x1: " << x << endl;
-        x = ( -1*b - sqrt(b*b - 4*a*c) ) / (2 * a);
+        x = (-1 * b - sqrt(b * b - 4 * a * c)) / (2 * a);
         cout << "x2: " << x << endl;
     }
-    else if((b*b - 4*a*c) == 0) 
+    else if ((b * b - 4 * a * c) == 0)
     {
-    	x = (-1*b) / (2*a);
-    	cout << "x: " << x << endl;
+        x = (-1 * b) / (2 * a);
+        cout << "x: " << x << endl;
     }
     else
     {
         cout << "Дискриминант меньше 0, корней нет." << endl;
     }
- 
+
     return 0;
 }
