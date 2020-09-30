bfcf291d26a1cc643343694e7ddc691a25b31542
Vadim <retroarefobia@gmail.com>
2020-09-30 21:06:09 +0300

    add task 09

---
+++ Practice/09/C++/9_meeting.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+/* Задание 9 - Встреча */
+
+using namespace std;
+
+int get_min(int h, int m)
+{
+    if (h < 0 || h > 23 || m < 0 || m > 59)
+    {
+        cout << "Неверное время. Повторите ввод." << endl;
+    }
+    else
+    {
+        return h * 60 + m;
+    }
+}
+
+int main()
+{
+    cout << "9. Встреча" << endl;
+    string s1, s2;
+
+    cin >> s1;
+    cin >> s2;
+
+    int t1 = stoi(s1.substr(0, s1.find(":")));
+    int t12 = stoi(s1.substr(s1.find(":") + 1, s1.length() - 1));
+    int t2 = stoi(s2.substr(0, s2.find(":")));
+    int t22 = stoi(s2.substr(s2.find(":") + 1, s2.length() - 1));
+
+    int m1 = get_min(t1,t12);
+    int m2 = get_min(t2, t22);
+
+    if (abs(m1 - m2) <= 15)
+        cout << "Встреча состоится" << endl;
+    else
+        cout << "Встреча не состоится" << endl;
+
+}
---
+++ Practice/09/Python/9_meeting.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# Задание 9 - Встреча
+
+class TimeNotValid(Exception):
+    pass
+
+
+def parse(inp):
+    h, m = map(int, inp.split(':'))
+
+    if (h not in range(0, 24)) or (m not in range(0, 60)):
+        raise TimeNotValid('Неверный формат времени!')
+    else:
+        return h * 60 + m
+
+print("9. Встреча")
+
+t1 = parse(input('Время первого человека:'))
+t2 = parse(input('Время второго человека:'))
+
+if abs(t1 - t2) <= 15:
+    print('Встреча состоится')
+else:
+    print('Встреча не состоится')
